2e5f5c272e79a14654c40d35ba61731768a89c20
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-10 22:07:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function writeProgram(){
+	if(confirm("Do you want to proceed?")){
+		alert("You clicked OK. Proceeding...");
+	}else{
+		alert("You clicked Cancel. Exiting...");
+	}
+}
+
+writeProgram();
